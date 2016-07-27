05e1de78033a3c9d5278fd86d838eed6ee32332c
Guzman Monne <guzmonne@hotmail.com>
2016-07-27 15:25:47 -0300

    turned Item component into a presentational component

--- src/components/menus/Item.js
+++ src/components/menus/Item.js
@@ -1,87 +1,51 @@
 import React, {PropTypes as T} from 'react'
 import classnames from 'classnames'
 
-class Item extends React.Component {
-	constructor(){
-		super()
-		this.getClassNames = this.getClassNames.bind(this)
-		this.spanPureMenuItem = this.spanPureMenuItem.bind(this)
-		this.anchorPureMenuItem = this.anchorPureMenuItem.bind(this)
-		this.getPureMenuItem = this.getPureMenuItem.bind(this)
-		this.state = {
-			hovering: false,
-		}
-	}
+const getClassNames = ({
+		selected,
+		disabled,
+		heading,
+		className,
+		items,
+		divided,
+		dividedHorizontal
+	}) => classnames(className, {
+		'pure-menu-item': !heading,
+		'pure-menu-heading': !!heading,
+		'pure-menu-selected': !heading && !!selected && !disabled,
+		'pure-menu-disabled': !heading && !selected && !!disabled,
+		'pure-menu-has-children': !!items,
+		'pure-menu-allow-hover': !!items,
+		'pure-menu-item-divided': !!divided,
+		'pure-menu-item-divided-horizontal': !!dividedHorizontal,
+	})
 
-	getClassNames(){
-		const {
-			selected,
-			disabled,
-			heading,
-			className,
-			items,
-			divided,
-			dividedHorizontal
-		} = this.props
-		return classnames(className, {
-			'pure-menu-item': !heading,
-			'pure-menu-heading': !!heading,
-			'pure-menu-selected': !heading && !!selected && !disabled,
-			'pure-menu-disabled': !heading && !selected && !!disabled,
-			'pure-menu-has-children': !!items,
-			'pure-menu-allow-hover': !!items,
-			'pure-menu-item-divided': !!divided,
-			'pure-menu-item-divided-horizontal': !!dividedHorizontal,
-		})
-	}
+const getPureMenuItem = (props) => {
+	const {heading, label, items, children, onClick, href} = props
+	if (!!items) return buildPureMenuItems(props)
+	const pureMenuItemContent = !!label ? label : children
+	return !!heading ? 
+		<span onClick={onClick}>{pureMenuItemContent}</span> : 
+		<a className="pure-menu-link" href={href} onClick={onClick}>
+			{pureMenuItemContent}
+		</a>
+}
 
-	spanPureMenuItem(pureMenuItemContent){
-		const {onClick} = this.props
-		return <span onClick={onClick}>{pureMenuItemContent}</span>
-	}
+const buildPureMenuItems = ({items}) =>
+	<ul className="pure-menu-children">
+		{items.map((item, i) => <Item key={i} {...item} />)}
+	</ul>
 
-	anchorPureMenuItem(pureMenuItemContent){
-		const {href, onClick} = this.props
-		return (
-			<a className="pure-menu-link" href={href} onClick={onClick}>
-				{pureMenuItemContent}
-			</a>
-		)
-	}
-
-	getPureMenuItem(){
-		const {heading, label, items, children} = this.props
-		if (!!items) return this.buildPureMenuItems()
-		const pureMenuItemContent = !!label ? label : children
-		return !!heading ? 
-			this.spanPureMenuItem(pureMenuItemContent) : 
-			this.anchorPureMenuItem(pureMenuItemContent)
-	}
-
-	buildPureMenuItems(){
-		const {items} = this.props
-		const maxLength = items
-			.reduce((max, item) => max > item.label.length ? max : item.label.length, 0)
-		const width = `${maxLength * 8 + 60}px`
-		console.log(maxLength)
-		return (
-			<ul style={{width}} className="pure-menu-children">
-				{items.map((item, i) => <Item key={i} {...item} />)}
-			</ul>
-		)
-	}
-
-	render(){
-		const {label, href, items, children} = this.props
-		const classNames = this.getClassNames()
-		const pureMenuItem = this.getPureMenuItem()
-		return (
-			<li className={classNames}>
-				{!!items && <a href={href} className="pure-menu-link">{label}</a>}
-				{pureMenuItem}
-			</li>
-		)
-	}
+const Item = (props) => {
+	const {label, href, items, children} = props
+	const classNames = getClassNames(props)
+	const pureMenuItem = getPureMenuItem(props)
+	return (
+		<li className={classNames}>
+			{!!items && <a href={href} className="pure-menu-link">{label}</a>}
+			{pureMenuItem}
+		</li>
+	)
 }
 
 Item.propTypes = {
