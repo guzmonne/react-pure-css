67892031114075c6dc4caf905ca3137fea065f14
Guzman Monne <guzmonne@hotmail.com>
2016-07-27 23:03:57 -0300

    exported Item and MenuHeading component

--- src/components/menus/index.js
+++ src/components/menus/index.js
@@ -2,7 +2,9 @@ require('./menus.css')
 import React from 'react'
 import classnames from 'classnames'
 import DefaultMenu from './Menu.js'
+import Item from './Item.js'
+import MenuHeading from './MenuHeading.js'
 
 const Menu = DefaultMenu
 
-export {Menu}
+export {Menu, Item, MenuHeading}
